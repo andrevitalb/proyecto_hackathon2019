74656c3e9380c12f2a81300584d6438a59846d44
André Vital <santiago_vital@outlook.com>
2019-11-02 20:39:58 -0600

    Changed logo & highlight color

--- assets/js/main.js
+++ assets/js/main.js
@@ -1176,7 +1176,7 @@ $(document).ready(function () {
         $('.chart2').appear();
         $('.chart2').easyPieChart({
             easing: 'easeOutCirc',
-            barColor: '#ff214f',
+            barColor: '#FFCC33',
             trackColor: '#c7c7c7',
             scaleColor: false,
             scaleLength: 1,
@@ -1204,7 +1204,7 @@ $(document).ready(function () {
         $('.chart3').appear();
         $('.chart3').easyPieChart({
             easing: 'easeOutCirc',
-            barColor: '#ff214f',
+            barColor: '#FFCC33',
             trackColor: '',
             scaleColor: false,
             scaleLength: 1,
